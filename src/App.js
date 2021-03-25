0ad8b17cb2e4d58fb74eacbbb479417a31324ed1
Subhratanu <subhratanu.jisce@gmail.com>
2021-03-25 17:47:31 +0530

    installing dev-op

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./styles/main.scss";
+import "rsuite/dist/styles/rsuite-default.css";
 function App() {
   return (
     <div className="App">
